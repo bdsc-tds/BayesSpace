5d85a05e98677ff29f226cdef9e605f9464fd975
Senbai Kang <senbaikang@gmail.com>
2023-06-29 16:19:07 +0200

    feat: process images

--- R/readVisium.R
+++ R/readVisium.R
@@ -11,7 +11,7 @@
 #'   \href{https://support.10xgenomics.com/spatial-gene-expression/software/pipelines/latest/output/overview}{10X Genomics help page}.)
 #' @param fname File name of the h5 file. It should be inside \code{dirname}.
 #'   (By default "filtered_feature_bc_matrix.h5")
-#' @param fullres.image Single H&E brightfield image in either TIFF or JPG 
+#' @param fullres.image Single H&E brightfield image in either TIFF or JPG
 #'   format; used as input to "spaceranger count".
 #' @param tile.image.dir Path to the directory of tile images.
 #' @param scale.factor.fname File name of the scale factor.
@@ -50,66 +50,68 @@ NULL
 readVisium <- function(dirname, fullres.image = NULL, tile.image.dir = NULL,
                        init.backend = TRUE, shutdown.backend = TRUE,
                        cores = 1, num.spots = -1) {
-    spatial_dir <- file.path(dirname, "spatial")
-    matrix_dir <- file.path(dirname, "filtered_feature_bc_matrix")
+  spatial_dir <- file.path(dirname, "spatial")
+  matrix_dir <- file.path(dirname, "filtered_feature_bc_matrix")
 
-    if (!dir.exists(matrix_dir)) {
-        stop("Matrix directory does not exist:\n  ", matrix_dir)
+  if (!dir.exists(matrix_dir)) {
+    stop("Matrix directory does not exist:\n  ", matrix_dir)
+  }
+  if (!dir.exists(spatial_dir)) {
+    stop("Spatial directory does not exist:\n  ", spatial_dir)
+  }
+
+  rowData <- read.table(file.path(matrix_dir, "features.tsv.gz"), header = FALSE, sep = "\t")
+  colnames(rowData) <- c("gene_id", "gene_name", "feature_type")
+  rowData <- rowData[, c("gene_id", "gene_name")]
+  rownames(rowData) <- scater::uniquifyFeatureNames(rowData$gene_id, rowData$gene_name)
+
+  .counts <- Matrix::readMM(file.path(matrix_dir, "matrix.mtx.gz"))
+  barcodes <- read.table(file.path(matrix_dir, "barcodes.tsv.gz"), header = FALSE, sep = "\t")
+  colData <- .read_spot_pos(spatial_dir, barcodes)
+  colnames(.counts) <- barcodes$V1
+  rownames(.counts) <- rownames(rowData)
+  .counts <- .counts[, rownames(colData)]
+
+  sce <- SingleCellExperiment(
+    assays = list(counts = .counts),
+    rowData = rowData,
+    colData = colData
+  )
+
+  # Remove spots with no reads for all genes.
+  sce <- sce[, Matrix::colSums(counts(sce)) > 0]
+
+  metadata(sce)$BayesSpace.data <- list()
+  metadata(sce)$BayesSpace.data$platform <- "Visium"
+  metadata(sce)$BayesSpace.data$is.enhanced <- FALSE
+
+  if (!is.null(fullres.image)) {
+    if (!file.exists(fullres.image)) {
+      stop("Full resolution image does not exist:\n  ", fullres.image)
     }
-    if (!dir.exists(spatial_dir)) {
-        stop("Spatial directory does not exist:\n  ", spatial_dir)
+
+    scale_factor_fname <- file.path(spatial_dir, "scalefactors_json.json")
+    if (!file.exists(scale_factor_fname)) {
+      stop("Scale factor file does not exist:\n  ", scale_factor_fname)
     }
 
-    rowData <- read.table(file.path(matrix_dir, "features.tsv.gz"), header = FALSE, sep = "\t")
-    colnames(rowData) <- c("gene_id", "gene_name", "feature_type")
-    rowData <- rowData[, c("gene_id", "gene_name")]
-    rownames(rowData) <- scater::uniquifyFeatureNames(rowData$gene_id, rowData$gene_name)
+    if (num.spots <= 0) {
+      num.spots <- dim(sce)[2]
+    } else {
+      sce <- sce[, seq_len(num.spots)]
+    }
 
-    .counts <- Matrix::readMM(file.path(matrix_dir, "matrix.mtx.gz"))
-    barcodes <- read.table(file.path(matrix_dir, "barcodes.tsv.gz"), header = FALSE, sep = "\t")
-    colData <- .read_spot_pos(spatial_dir, barcodes)
-    colnames(.counts) <- barcodes$V1
-    rownames(.counts) <- rownames(rowData)
-    .counts <- .counts[, rownames(colData)]
-
-    sce <- SingleCellExperiment(
-        assays = list(counts = .counts),
-        rowData = rowData,
-        colData = colData
+    flattened_tiles <- create_tiles(
+      sce, fullres.image, scale_factor_fname,
+      tile.image.dir, num.spots, init.backend,
+      shutdown.backend, cores
     )
 
-    # Remove spots with no reads for all genes.
-    sce <- sce[, Matrix::colSums(counts(sce)) > 0]
+    metadata(sce)$BayesSpace.data$spot_image <- flattened_tiles$spot
+    metadata(sce)$BayesSpace.data$subspot_image <- flattened_tiles$subspot
+  }
 
-    metadata(sce)$BayesSpace.data <- list()
-    metadata(sce)$BayesSpace.data$platform <- "Visium"
-    metadata(sce)$BayesSpace.data$is.enhanced <- FALSE
-    
-    if (!is.null(fullres.image)) {
-      if (!file.exists(fullres.image)) {
-        stop("Full resolution image does not exist:\n  ", fullres.image)
-      }
-      
-      scale_factor_fname <- file.path(spatial_dir, "scalefactors_json.json")
-      if (!file.exists(scale_factor_fname)) {
-        stop("Scale factor file does not exist:\n  ", scale_factor_fname)
-      }
-      
-      if(num.spots <= 0) {
-        num.spots <- dim(sce)[2]
-      } else {
-        sce <- sce[, seq_len(num.spots)]
-      }
-      
-      flattened_tiles <- create_tiles(sce, fullres.image, scale_factor_fname,
-                                      tile.image.dir, num.spots, init.backend,
-                                      shutdown.backend, cores)
-      
-      metadata(sce)$BayesSpace.data$spot_image <- flattened_tiles$spot
-      metadata(sce)$BayesSpace.data$subspot_image <- flattened_tiles$subspot
-    }
-
-    sce
+  sce
 }
 
 #' @export
@@ -125,96 +127,98 @@ read10Xh5 <- function(dirname, fname = "filtered_feature_bc_matrix.h5",
                       fullres.image = NULL, tile.image.dir = NULL,
                       init.backend = TRUE, shutdown.backend = TRUE,
                       cores = 1, num.spots = -1) {
-    spatial_dir <- file.path(dirname, "spatial")
-    h5_file <- file.path(dirname, fname)
+  spatial_dir <- file.path(dirname, "spatial")
+  h5_file <- file.path(dirname, fname)
 
-    if (!dir.exists(spatial_dir)) {
-      stop("Spatial directory does not exist:\n  ", spatial_dir)
-    }
+  if (!dir.exists(spatial_dir)) {
+    stop("Spatial directory does not exist:\n  ", spatial_dir)
+  }
 
-    if (!file.exists(h5_file)) {
-      stop("H5 file does not exist:\n  ", h5_file)
-    }
+  if (!file.exists(h5_file)) {
+    stop("H5 file does not exist:\n  ", h5_file)
+  }
 
-    colData <- .read_spot_pos(spatial_dir)
+  colData <- .read_spot_pos(spatial_dir)
 
-    non.zero.indices <- .extract_indices(
-      h5read(h5_file, "matrix/indices"),
-      h5read(h5_file, "matrix/indptr")
-    )
+  non.zero.indices <- .extract_indices(
+    h5read(h5_file, "matrix/indices"),
+    h5read(h5_file, "matrix/indptr")
+  )
 
-    rowData <- data.frame(
-      gene_id = h5read(h5_file, "matrix/features/id"),
-      gene_name = h5read(h5_file, "matrix/features/name")
+  rowData <- data.frame(
+    gene_id = h5read(h5_file, "matrix/features/id"),
+    gene_name = h5read(h5_file, "matrix/features/name")
+  ) %>%
+    group_by(
+      gene_name
     ) %>%
-      group_by(
-        gene_name
-      ) %>%
-      mutate(
-        idx = 1:n(),
-        row_name = case_when(
-          max(idx) > 1 ~ paste(gene_name, gene_id, sep = "_"),
-          TRUE ~ gene_name
-        )
-      ) %>%
-      column_to_rownames("row_name") %>%
-      select(
-        -idx
+    mutate(
+      idx = 1:n(),
+      row_name = case_when(
+        max(idx) > 1 ~ paste(gene_name, gene_id, sep = "_"),
+        TRUE ~ gene_name
       )
-
-    .counts <- sparseMatrix(
-      i = non.zero.indices$i,
-      j = non.zero.indices$j,
-      x = h5read(h5_file, "matrix/data"),
-      dims = h5read(h5_file, "matrix/shape"),
-      dimnames = list(
-        rownames(rowData),
-        h5read(h5_file, "matrix/barcodes")
-      ),
-      index1 = FALSE
-    )
-    .counts <- .counts[, rownames(colData)]
-
-    sce <- SingleCellExperiment(
-      assays = list(
-        counts = .counts
-      ),
-      rowData = rowData,
-      colData = colData
+    ) %>%
+    column_to_rownames("row_name") %>%
+    select(
+      -idx
     )
 
-    # Remove spots with no reads for all genes.
-    sce <- sce[, Matrix::colSums(counts(sce)) > 0]
+  .counts <- sparseMatrix(
+    i = non.zero.indices$i,
+    j = non.zero.indices$j,
+    x = h5read(h5_file, "matrix/data"),
+    dims = h5read(h5_file, "matrix/shape"),
+    dimnames = list(
+      rownames(rowData),
+      h5read(h5_file, "matrix/barcodes")
+    ),
+    index1 = FALSE
+  )
+  .counts <- .counts[, rownames(colData)]
 
-    metadata(sce)$BayesSpace.data <- list()
-    metadata(sce)$BayesSpace.data$platform <- "Visium"
-    metadata(sce)$BayesSpace.data$is.enhanced <- FALSE
-    
-    if (!is.null(fullres.image)) {
-      if (!file.exists(fullres.image)) {
-        stop("Full resolution image does not exist:\n  ", fullres.image)
-      }
-      
-      scale_factor_fname <- file.path(spatial_dir, "scalefactors_json.json")
-      if (!file.exists(scale_factor_fname)) {
-        stop("Scale factor file does not exist:\n  ", scale_factor_fname)
-      }
-      
-      if(num.spots <= 0) {
-        num.spots <- dim(sce)[2]
-      } else {
-        sce <- sce[, seq_len(num.spots)]
-      }
-      
-      flattened_tiles <- create_tiles(sce, fullres.image, scale_factor_fname,
-                                      tile.image.dir, num.spots, init.backend,
-                                      shutdown.backend, cores)
-      
-      metadata(sce)$BayesSpace.data$spot_image <- flattened_tiles$spot
-      metadata(sce)$BayesSpace.data$subspot_image <- flattened_tiles$subspot
+  sce <- SingleCellExperiment(
+    assays = list(
+      counts = .counts
+    ),
+    rowData = rowData,
+    colData = colData
+  )
+
+  # Remove spots with no reads for all genes.
+  sce <- sce[, Matrix::colSums(counts(sce)) > 0]
+
+  metadata(sce)$BayesSpace.data <- list()
+  metadata(sce)$BayesSpace.data$platform <- "Visium"
+  metadata(sce)$BayesSpace.data$is.enhanced <- FALSE
+
+  if (!is.null(fullres.image)) {
+    if (!file.exists(fullres.image)) {
+      stop("Full resolution image does not exist:\n  ", fullres.image)
     }
 
-    sce
+    scale_factor_fname <- file.path(spatial_dir, "scalefactors_json.json")
+    if (!file.exists(scale_factor_fname)) {
+      stop("Scale factor file does not exist:\n  ", scale_factor_fname)
+    }
+
+    if (num.spots <= 0) {
+      num.spots <- dim(sce)[2]
+    } else {
+      sce <- sce[, seq_len(num.spots)]
+    }
+
+    flattened_tiles <- create_tiles(
+      sce, fullres.image, scale_factor_fname,
+      tile.image.dir, num.spots, init.backend,
+      shutdown.backend, cores
+    )
+
+    metadata(sce)$BayesSpace.data$spot_image <- flattened_tiles$spot
+    metadata(sce)$BayesSpace.data$subspot_image <- flattened_tiles$subspot
+  }
+
+  sce
 }
 
 #' @export
@@ -226,10 +230,10 @@ create_tiles <- function(sce, fullres.image, scale.factor.fname,
                          cores = 1) {
   if (!file.exists(fullres.image)) stop("Full resolution image does not exist:\n", fullres.image)
   if (!file.exists(scale.factor.fname)) stop("Scale factor file does not exist:\n", scale.factor.fname)
-  
+
   if (is.null(tile.image.dir)) tile.image.dir <- tempdir(check = TRUE)
   message(paste0("Tiles are stored in ", tile.image.dir))
-  
+
   .barcodes <- as.vector(colData(sce)[seq_len(num.spots), "barcode"])
   flattened_tiles <- get_spot_subspot_tiles_from_image(
     .barcodes,
@@ -237,10 +241,10 @@ create_tiles <- function(sce, fullres.image, scale.factor.fname,
     fromJSON(file = scale.factor.fname)$spot_diameter_fullres / 2,
     fullres.image, tile.image.dir, init.backend, shutdown.backend, cores
   )
-  
+
   colnames(flattened_tiles$spot) <- .barcodes
   colnames(flattened_tiles$subspot) <- flattened_tiles$subspot_barcodes
-  
+
   flattened_tiles
 }
 
@@ -251,21 +255,21 @@ counts2h5 <- function(dirname) {
   h5_file <- file.path(dirname, "filtered_feature_bc_matrix.h5")
   spatial_dir <- file.path(dirname, "spatial")
   matrix_dir <- file.path(dirname, "filtered_feature_bc_matrix")
-  
+
   if (file.exists(h5_file)) {
     stop("H5 file exists:\n ", h5_file)
   }
-  
+
   if (!dir.exists(matrix_dir)) {
     stop("Matrix directory does not exist:\n  ", matrix_dir)
   }
   if (!dir.exists(spatial_dir)) {
     stop("Spatial directory does not exist:\n  ", spatial_dir)
   }
-  
+
   rowData <- read.table(file.path(matrix_dir, "features.tsv.gz"), header = FALSE, sep = "\t")
   colnames(rowData) <- c("gene_id", "gene_name", "feature_type")
-  
+
   .counts <- readMM(file.path(matrix_dir, "matrix.mtx.gz"))
   counts <- matrix(
     as.integer(as.matrix(.counts)),
@@ -274,24 +278,24 @@ counts2h5 <- function(dirname) {
   .counts <- as(.counts, "CsparseMatrix")
   barcodes <- read.table(file.path(matrix_dir, "barcodes.tsv.gz"), header = FALSE, sep = "\t")
   colData <- .read_spot_pos(spatial_dir, barcodes)
-  
+
   h5createFile(h5_file)
-  
+
   h5createGroup(h5_file, "matrix")
-  
+
   h5write(barcodes[[1]], h5_file, "matrix/barcodes")
   h5write(counts[counts > 0], h5_file, "matrix/data")
-  
+
   h5createGroup(h5_file, "matrix/features")
   h5write("genome", h5_file, "/matrix/features/_all_tag_keys")
   h5write(rowData$feature_type, h5_file, "/matrix/features/feature_type")
   h5write(rowData$gene_id, h5_file, "/matrix/features/id")
   h5write(rowData$gene_name, h5_file, "/matrix/features/name")
-  
+
   h5write(.counts@i, h5_file, "matrix/indices")
   h5write(.counts@p, h5_file, "matrix/indptr")
   h5write(dim(counts), h5_file, "matrix/shape")
-  
+
   NULL
 }
 
@@ -312,14 +316,14 @@ counts2h5 <- function(dirname) {
 #' @importFrom tidyr uncount
 .read_spot_pos <- function(dirname, barcodes = NULL) {
   if (file.exists(file.path(dirname, "tissue_positions_list.csv"))) {
-      message("Inferred Space Ranger version < V2.0")
-      colData <- read.csv(file.path(dirname, "tissue_positions_list.csv"), header = FALSE)
-      colnames(colData) <- c("barcode", "in_tissue", "array_row", "array_col", "pxl_row_in_fullres", "pxl_col_in_fullres")
+    message("Inferred Space Ranger version < V2.0")
+    colData <- read.csv(file.path(dirname, "tissue_positions_list.csv"), header = FALSE)
+    colnames(colData) <- c("barcode", "in_tissue", "array_row", "array_col", "pxl_row_in_fullres", "pxl_col_in_fullres")
   } else if (file.exists(file.path(dirname, "tissue_positions.csv"))) {
-      message("Inferred Space Ranger version >= V2.0")
-      colData <- read.csv(file.path(dirname, "tissue_positions.csv"))
+    message("Inferred Space Ranger version >= V2.0")
+    colData <- read.csv(file.path(dirname, "tissue_positions.csv"))
   } else {
-      stop("No file for spot positions found in ", dirname)
+    stop("No file for spot positions found in ", dirname)
   }
 
   if (!is.null(barcodes)) {
@@ -329,14 +333,14 @@ counts2h5 <- function(dirname) {
       by = c("barcode" = "V1")
     )
   }
-  
+
   # Sanity check.
   if (
     abs(cor(colData$array_row, colData$pxl_row_in_fullres)) < abs(cor(colData$array_row, colData$pxl_col_in_fullres)) &&
-    abs(cor(colData$array_col, colData$pxl_col_in_fullres)) < abs(cor(colData$array_col, colData$pxl_row_in_fullres))
+      abs(cor(colData$array_col, colData$pxl_col_in_fullres)) < abs(cor(colData$array_col, colData$pxl_row_in_fullres))
   ) {
     message("Warning! The coordinates with indices and pixels do not match. Swapping the pixel values between the row and column...")
-    
+
     colData <- transform(
       colData,
       pxl_row_in_fullres = pxl_col_in_fullres,
@@ -365,24 +369,24 @@ counts2h5 <- function(dirname) {
 #' @importFrom tibble as_tibble
 #' @importFrom tidyr uncount
 .extract_indices <- function(idx, new.start, zero.based = TRUE) {
-    if (length(idx) < 1) {
-        return(NULL)
-    }
+  if (length(idx) < 1) {
+    return(NULL)
+  }
 
-    idx.cnts <- do.call(
-        rbind,
-        lapply(
-            seq_len(length(new.start))[-1],
-            function(x) c(x - ifelse(zero.based, 2, 1), new.start[[x]] - new.start[[x - 1]])
-        )
+  idx.cnts <- do.call(
+    rbind,
+    lapply(
+      seq_len(length(new.start))[-1],
+      function(x) c(x - ifelse(zero.based, 2, 1), new.start[[x]] - new.start[[x - 1]])
     )
-    colnames(idx.cnts) <- c("id", "n")
+  )
+  colnames(idx.cnts) <- c("id", "n")
 
-    return(
-        list(
-            i = idx,
-            j = as.integer(uncount(as_tibble(idx.cnts), n)[[1]]),
-            new.start = new.start
-        )
+  return(
+    list(
+      i = idx,
+      j = as.integer(uncount(as_tibble(idx.cnts), n)[[1]]),
+      new.start = new.start
     )
+  )
 }
